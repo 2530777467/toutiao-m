f9bd0afed8f158cdde70215e506563428f733e10
2530777467 <2530777467@qq.com>
2021-07-15 09:29:29 +0800

    2

--- src/main.js
+++ src/main.js
@@ -3,10 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import Vant from 'vant'   //加载Vant组件库
-import 'vant/lib/index.css'  
+import Vant from 'vant'   //加载 Vant 组件库
+import 'vant/lib/index.css'  //加载 Vant 组件库样式
 
-Vue.use(Vant)
+Vue.use(Vant)  //全局注册 Vant 中的组件
 
 Vue.config.productionTip = false
 
